ffa6615c5239cd8f7e2b0ac8072b67ec0731918e
emm3fg <eric.m.mcdaniel@gmail.com>
2015-06-02 23:59:07 -0400

    Adding initial files

---
+++ content.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+		// Post thumbnail.
+		twentyfifteen_post_thumbnail();
+	?>
+
+	<header class="entry-header">
+		<?php
+			if ( is_single() ) :?>
+				<h2 class="category-title"><?php the_category(' ');?></h2>
+				<div class="entry-title">
+					<?php the_title( '<h1>', '</h1>' );?>
+					<h3 class="entry-byline"><?php the_author_posts_link(); ?></h3>
+				</div>
+					<p class="word-count">
+						Word Count: <?php if(function_exists('word_count')) { word_count(); }?>
+					</p>
+				<?php
+			else :
+				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+			endif;
+		?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s', 'twentyfifteen' ),
+				the_title( '<span class="screen-reader-text">', '</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php
+		// Swipe instructions for mobile
+		if ( is_single() ) : ?>
+			<div class="swipe-instructions">Swipe across chapters &#8594;</div><?php
+		endif;
+	?>
+	
+	<?php
+		// Author bio.
+		if ( is_single() && get_the_author_meta( 'description' ) ) :
+			get_template_part( 'author-bio' );
+		endif;
+	?>
+
+	<footer class="entry-footer">
+		<?php twentyfifteen_entry_meta(); ?>
+		<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
